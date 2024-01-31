299da2aef85d8f040e9d3318d9bcd296a272945c
rahulbyadav <113384835+rahulbyadav@users.noreply.github.com>
2024-01-31 23:02:04 +0530

    Add files via upload

---
+++ CURA-Healthcare-Katalon-Web-Testing-main/Libs/internal/GlobalVariable.groovy
@@ -0,0 +1,42 @@
+package internal
+
+import com.kms.katalon.core.configuration.RunConfiguration
+import com.kms.katalon.core.main.TestCaseMain
+
+
+/**
+ * This class is generated automatically by Katalon Studio and should not be modified or deleted.
+ */
+public class GlobalVariable {
+     
+    /**
+     * <p></p>
+     */
+    public static Object URL
+     
+    /**
+     * <p></p>
+     */
+    public static Object User
+     
+    /**
+     * <p></p>
+     */
+    public static Object Password
+     
+
+    static {
+        try {
+            def selectedVariables = TestCaseMain.getGlobalVariables("default")
+			selectedVariables += TestCaseMain.getGlobalVariables(RunConfiguration.getExecutionProfile())
+            selectedVariables += TestCaseMain.getParsedValues(RunConfiguration.getOverridingParameters(), selectedVariables)
+    
+            URL = selectedVariables['URL']
+            User = selectedVariables['User']
+            Password = selectedVariables['Password']
+            
+        } catch (Exception e) {
+            TestCaseMain.logGlobalVariableError(e)
+        }
+    }
+}
